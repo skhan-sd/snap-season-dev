1a55d6565d0e924bb2dde17d275b84b322a09650
Safwaan Khan <skhan@seconddinner.com>
2026-02-26 12:26:44 -0800

    Fix Express wildcard route

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.post("/api/generate", async (req, res) => {
 // ===== SERVE FRONTEND =====
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
